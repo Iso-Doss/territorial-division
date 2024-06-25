c83350592f958587af0f174743910f388f52c031
Iso-Doss <dosou.israel48@gmail.com>
2024-06-25 14:58:22 +0100

    Updated install script file.

--- install.sh
+++ install.sh
@@ -1,25 +1,29 @@
 #!/usr/bin/env php
 
+# ANSI color code for red
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 echo "Running territorial division install script..."
 
 if [ -f "artisan" ]; then
     echo "Artisan file found. Running custom installation script..."
 
-    php artisan make:cache-table --force
-    php artisan make:session-table --force
-    php artisan make:queue-table --force
-    php artisan make:queue-failed-table --force
-    php artisan make:queue-batches-table --force
-    php artisan make:notifications-table --force
+    php artisan make:cache-table
+    php artisan make:session-table
+    php artisan make:queue-table
+    php artisan make:queue-failed-table
+    php artisan make:queue-batches-table
+    php artisan make:notifications-table
 
-    php artisan telescope:install --force
-    php artisan install:api --passport --force
-    php artisan vendor:publish --provider="Laravel\Pulse\PulseServiceProvider" --force
-    php artisan vendor:publish --provider="Spatie\Permission\PermissionServiceProvider" --force
-    php artisan vendor:publish --provider="Spatie\Activitylog\ActivitylogServiceProvider" --tag="activitylog-migrations" --force
-    php artisan vendor:publish --provider="Spatie\Activitylog\ActivitylogServiceProvider" --tag="activitylog-config" --force
+    php artisan telescope:install
+    php artisan install:api --passport
+    php artisan vendor:publish --provider="Laravel\Pulse\PulseServiceProvider"
+    php artisan vendor:publish --provider="Spatie\Permission\PermissionServiceProvider"
+    php artisan vendor:publish --provider="Spatie\Activitylog\ActivitylogServiceProvider" --tag="activitylog-migrations"
+    php artisan vendor:publish --provider="Spatie\Activitylog\ActivitylogServiceProvider" --tag="activitylog-config"
 
     php artisan migrate
 else
-    echo "Artisan file not found. Skipping custom installation script."
+    echo -e "${RED}Artisan file not found. Skipping custom installation script.${NC}"
 fi
