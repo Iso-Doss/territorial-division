537e55f88b6d92448e75de95f9690ad21dd6d045
Iso-Doss <dosou.israel48@gmail.com>
2024-06-25 12:38:20 +0100

    Create TerritorialDivisionServiceProvider class

---
+++ src/TerritorialDivisionProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace IsoDoss\TerritorialDivision;
+
+use Illuminate\Support\ServiceProvider;
+
+class TerritorialDivisionProvider extends ServiceProvider
+{
+	/**
+	 * Bootstrap any package services.
+	 *
+	 * @return void
+	 */
+	public function boot(): void
+	{
+		//
+	}
+
+	/**
+	 * Register any package services.
+	 *
+	 * @return void
+	 */
+	public function register(): void
+	{
+		//
+	}
+}
