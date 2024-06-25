3e021943c1e0bee93f70e4fdd0ced71f1c54f8e9
Iso-Doss <dosou.israel48@gmail.com>
2024-06-25 14:26:11 +0100

    Updated install script file.

--- install.sh
+++ install.sh
@@ -2,18 +2,24 @@
 
 echo "Running territorial division install script..."
 
-php artisan make:cache-table --force
-php artisan make:session-table --force
-php artisan make:queue-table --force
-php artisan make:queue-failed-table --force
-php artisan make:queue-batches-table --force
-php artisan make:notifications-table --force
+if [ -f "artisan" ]; then
+    echo "Artisan file found. Running custom installation script..."
 
-php artisan telescope:install --force
-php artisan install:api --passport --force
-php artisan vendor:publish --provider="Laravel\Pulse\PulseServiceProvider" --force
-php artisan vendor:publish --provider="Spatie\Permission\PermissionServiceProvider" --force
-php artisan vendor:publish --provider="Spatie\Activitylog\ActivitylogServiceProvider" --tag="activitylog-migrations" --force
-php artisan vendor:publish --provider="Spatie\Activitylog\ActivitylogServiceProvider" --tag="activitylog-config" --force
+    php artisan make:cache-table --force
+    php artisan make:session-table --force
+    php artisan make:queue-table --force
+    php artisan make:queue-failed-table --force
+    php artisan make:queue-batches-table --force
+    php artisan make:notifications-table --force
 
-php artisan migrate
+    php artisan telescope:install --force
+    php artisan install:api --passport --force
+    php artisan vendor:publish --provider="Laravel\Pulse\PulseServiceProvider" --force
+    php artisan vendor:publish --provider="Spatie\Permission\PermissionServiceProvider" --force
+    php artisan vendor:publish --provider="Spatie\Activitylog\ActivitylogServiceProvider" --tag="activitylog-migrations" --force
+    php artisan vendor:publish --provider="Spatie\Activitylog\ActivitylogServiceProvider" --tag="activitylog-config" --force
+
+    php artisan migrate
+else
+    echo "Artisan file not found. Skipping custom installation script."
+fi
